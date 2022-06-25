ef0fb517d0e5cc8f9d500ee29c136f0f8abb6bf1
Diem Quynh Tran <dquynhcn1.uet@gmail.com>
2022-06-25 23:25:22 +0700

    Add readme file

---
+++ README.md
@@ -0,0 +1,9 @@
+1. SLICE
+2. MAP
+*Syntax:     
+    var listMap map[string]int = map[string]int{ }
+Or:
+    listMap := make(map[string]int)
+With: string/int are type of key and value
+
+
--- main.go
+++ main.go
@@ -1,6 +1,35 @@
 package main
-import "fmt"
+ 
+import (
+    "fmt"
+    //"bufio"
+    //"os"
+    //"strconv"
+
+)
 
 func main() {
-	fmt.Println("Hello");
+    // //input
+    // scanner := bufio.NewScanner(os.Stdin)
+    // scanner.Scan()
+    // input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+    // fmt.Print(2022- input)
+    
+    //slide
+    var listMap map[string]int = map[string]int{
+        "apple": 5,
+        "bear": 6,
+        "orange": 10,
+    }
+
+    val, ok := listMap["bear"]
+
+    fmt.Println(val, ok)
+
+    //mapTwo := make(map[string]int)
+
+
+
+
+
 }
