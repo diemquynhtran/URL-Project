38c77015bcc2bf5f198136809b7fd48c4612d33e
Diem Quynh Tran <dquynhcn1.uet@gmail.com>
2022-06-26 12:14:23 +0700

    Add readme3

--- README.md
+++ README.md
@@ -1,11 +1,38 @@
-# SLICE
-# MAP
-## 1. Cú pháp
+# PACKET
+## fmt
+# ARRAY
+- Kiểu tham trị
 ```Golang
-    var listMap map[string]int = map[string]int{ }
+    var x [2]int = [2]int{1,2}
+    y := x
+    y[0] = 100
+    fmt.Println(x,y)
+    //Output: [1 2] [100 2]
+```
+# SLICE
+- Kiểu tham chiếu
+```Golang
+    var x []int = []int{1,2}
+    y := x
+    y[0] = 100
+    fmt.Println(x,y)
+    //Output: [100 2] [100 2]
+```
+# MAP
+## Syntax
+```Golang
+    var listMap map[string]int = map[string]int{...}
 Or:
     listMap := make(map[string]int)
+String/int are type of key/value
+
 ```
-string/int are type of key and value
+- Check element:
+```Golang
+    val, ok := listMap[key]
+```
+if key isn't exist ok=false
+# FUNCTION
+- defer dùng để trì hoãn câu lệnh đến cuối cùng mới thực hiện (trước return)
 
 
--- main.go
+++ main.go
@@ -9,13 +9,15 @@ import (
 )
 
 func main() {
-    // //input
-    // scanner := bufio.NewScanner(os.Stdin)
-    // scanner.Scan()
-    // input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
-    // fmt.Print(2022- input)
-    
-    //slide
+
+    /*input
+    scanner := bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+    input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+    fmt.Print(2022- input)
+    */
+
+    /* MAP
     var listMap map[string]int = map[string]int{
         "apple": 5,
         "bear": 6,
@@ -26,10 +28,21 @@ func main() {
 
     fmt.Println(val, ok)
 
-    //mapTwo := make(map[string]int)
-
-
-
-
+    mapTwo := make(map[string]int)
+    */
+    var x [2]int = [2]int{1,2}
+    y := x
+    y[0] = 100
+    fmt.Println(x,y)
 
 }
+
+func absolute(x int) (result int) {
+    if x < 0 {
+        result = -x
+        return
+    }
+    result = x
+    return 
+}
+
